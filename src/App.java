4ed019278734610f8f3dcba0a5e0345554a8cd84
Noah Söng <noah.song@elev.ga.ntig.se>
2023-11-27 08:57:20 +0100

    gjort a) och b)

---
+++ src/App.java
@@ -0,0 +1,21 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // double omkretsen= omkretsCirkel(9);
+        // System.out.println("Omkretsen blir "+omkretsen);
+
+    ritaLinje(23);
+    }
+// static double omkretsCirkel (double radie)
+// {
+// double omkrets=radie*6.28;
+// return omkrets;
+// }
+
+static void ritaLinje (int längd)
+{
+for (int i = 0; i < längd; i++) {
+ System.out.print("-");
+    
+}
+}
+}
